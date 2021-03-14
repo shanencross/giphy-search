f7ca0f5edd71e033fb5747d4284c2d939c0417dd
Shanen Cross <shanencross@yahoo.com>
2021-03-14 15:16:30 -0700

    Try out getting information from a request to Giphy
    Co-authored-by: Faisal Rana <inquisitive@gmail.com>
    Co-authored-by: Garrett Brown <garrettpaulbrown@gmail.com>

--- src/main.js
+++ src/main.js
@@ -2,3 +2,23 @@
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
+
+
+
+let request = new XMLHttpRequest();
+const url = `https://api.giphy.com/v1/gifs/search?api_key=${process.env.API_KEY}&q=ryan gosling&limit=5&offset=0&rating=g&lang=en`;
+
+request.onreadystatechange = function() {
+    if (this.readyState === 4 && this.status === 200)
+    {
+        const response = JSON.parse(this.responseText);
+        getElements(response);
+    }
+}
+
+request.open("GET", url, true);
+request.send();
+
+function getElements(response) {
+    console.log(response.data[0].url)
+}
