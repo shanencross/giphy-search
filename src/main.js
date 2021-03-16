87f2dcb665390eb788512ff4a98cb37807dbef2f
Shanen Cross <shanencross@yahoo.com>
2021-03-15 19:07:26 -0700

    Fix appending of img tag and remove .ready
    Co-authored-by: Garrett Brown <garrettpaulbrown@gmail.com>

--- src/main.js
+++ src/main.js
@@ -3,16 +3,13 @@ import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 
-$(document).ready(function() {
-
-
-
+$("#requestButton").on("click", function() {
     let request = new XMLHttpRequest();
     const url = `https://api.giphy.com/v1/gifs/search?api_key=${process.env.API_KEY}&q=ryan gosling&limit=5&offset=0&rating=g&lang=en`;
 
     request.onreadystatechange = function() {
         if (this.readyState === 4 && this.status === 200)
-    {
+        {
             const response = JSON.parse(this.responseText);
             getElements(response);
         }
@@ -20,9 +17,8 @@ $(document).ready(function() {
 
     request.open("GET", url, true);
     request.send();
-
-    function getElements(response) {
-        $('#photo').text(`<img="${response.data[0].url}">`);
-    }   
-
 });
+
+function getElements(response) {
+    $("#photo").append(`<img src="${response.data[0].images.original.url}">`);
+}   
